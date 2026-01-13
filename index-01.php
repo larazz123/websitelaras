89547976556d34227e11eb6b744b57f0b595cc60
larazz123 <laraswidyaningrum2004@gmail.com>
2026-01-13 22:18:59 +0700

    Add files via upload

---
+++ index-01.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Web Responsif PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-sm-12 bg-primary text-white p-3">
+            Sidebar
+        </div>
+        <div class="col-md-8 col-sm-12 bg-light p-3">
+            Konten Utama
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
